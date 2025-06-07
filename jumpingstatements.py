1f2445cd3defafb46b3f66b09f1d58233c7000bf
Bhanusree-2004 <bhanu302030@gmail.com>
2025-06-07 11:29:36 +0530

    Add files via upload

---
+++ jumpingstatements.py
@@ -0,0 +1,15 @@
+for i in "python":
+    if i == 'o':
+        break
+    print(i)
+
+for j in "hellomine":
+    if j == "m":
+        continue
+    print(j)
+
+for k in "helloworld":
+    if k == "helloworld":
+        pass
+    print(k)    
+
